f77f13932df31695631d8280d643ae2ce5ab1286
Scott Barnes <scott.barnes.g@gmail.com>
2025-11-02 16:34:45 -0500

    Fix path to files in create-service.sh

--- scripts/create-service.sh
+++ scripts/create-service.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Copy base file to the actual file
-cp power-monitor.base.service power-monitor.service
+cp scripts/power-monitor.base.service scripts/power-monitor.service
 # Set current user as user to run service as
 echo "Setting user $USER as power monitor user..."
-sed -i "/^User=/ s/$/$USER\n/" power-monitor.service
+sed -i "/^User=/ s/$/$USER\n/" scripts/power-monitor.service
 # Use current path to set execution path for service file
 echo "Setting execution path for power monitor service..."
 path=$(cd ../ && pwd)
-echo "ExecStart=$path/run.sh" >> power-monitor.service
-echo "WorkingDirectory=$path" >> power-monitor.service
+echo "ExecStart=$path/run.sh" >> scripts/power-monitor.service
+echo "WorkingDirectory=$path" >> scripts/power-monitor.service
 # Copy service file to systemd directory
 echo "Copying service file to systemd service directory..."
-sudo cp power-monitor.service /etc/systemd/system/
+sudo cp scripts/power-monitor.service /etc/systemd/system/
 # Enable and start service
 echo "Enabling and starting service..."
 sudo systemctl enable power-monitor
